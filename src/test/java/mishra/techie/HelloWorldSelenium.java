140cddb6035422d9bd5898d950ec61612cd0a0e7
Ashutosh <mishra.techie@gmail.com>
2016-01-12 21:30:41 +0530

    Removed Warning

--- src/test/java/mishra/techie/HelloWorldSelenium.java
+++ src/test/java/mishra/techie/HelloWorldSelenium.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloWorldSelenium {
